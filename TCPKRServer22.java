9faa0b624bbc28650c1f0959d728d3aa0bbd9e57
nshein1 <93554048+nshein1@users.noreply.github.com>
2022-03-03 15:46:05 -0500

    readWebsite

--- TCPKRServer22.java
+++ TCPKRServer22.java
@@ -21,8 +21,24 @@ class TCPKRServer22 {
              new DataOutputStream(connectionSocket.getOutputStream());
 
            clientSentence = inFromClient.readLine();
-           System.out.println("String received");
+           
+           System.out.println("String received\n" + clientSentence);
+           
+           
 
+           String inputLine;
+           URL obj = new URL(clientSentence);
+           HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+           con.setRequestMethod("GET");
+           con.setRequestProperty("User-Agent","Mozilla/5.0");
+           BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream())); 
+                        while ((inputLine = in.readLine()) != null) 
+                           System.out.println(inputLine);   
+
+           System.out.println("W1 request sent at: " + java.time.LocalTime.now());
+
+           System.out.println("W1 response received at: " + java.time.LocalTime.now());
            capitalizedSentence = clientSentence.toUpperCase() + '\n';
 
            outToClient.writeBytes(capitalizedSentence);
