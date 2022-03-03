388198c47b65bd908541767ce26386ab5588ece6
alexTU1 <95954794+alexTU1@users.noreply.github.com>
2022-03-03 14:11:30 -0800

    adding time check

--- TCPKRServer22.java
+++ TCPKRServer22.java
@@ -1,12 +1,20 @@
 import java.io.*;
 import java.net.*;
+import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+//import java.time.temporal.ChronoUnit;
+
 
 class TCPKRServer22 {
 
   public static void main(String argv[]) throws Exception
     {
       String clientSentence;
+      //String clientSentence2;
       String capitalizedSentence;
+     // String capitalizedSentence2;
+     
+
 
       try (ServerSocket welcomeSocket = new ServerSocket(12211)) {
  
@@ -21,11 +29,11 @@ class TCPKRServer22 {
              new DataOutputStream(connectionSocket.getOutputStream());
 
            clientSentence = inFromClient.readLine();
+           //clientSentence2 = inFromClient.readLine();
            
            System.out.println("String received\n" + clientSentence);
-           int i = 0;
+          
            
-
            String inputLine;
            URL obj = new URL(clientSentence);
            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
@@ -33,20 +41,25 @@ class TCPKRServer22 {
            con.setRequestProperty("User-Agent","Mozilla/5.0");
            BufferedReader in = new BufferedReader(
                 new InputStreamReader(con.getInputStream())); 
-           System.out.println("W1 request sent at: " + java.time.LocalTime.now());
-                        while ((inputLine = in.readLine()) != null) {
-                          
-                          String data[] = new data[1024];
-                          data[i] = inputLine;
-                          i++ 
-                        }
+                        while ((inputLine = in.readLine()) != null) 
+                           System.out.println(inputLine);   
 
+          LocalTime start = LocalTime.now();
+          LocalTime end = LocalTime.now();
+         
+          final long difference = end.getLong(ChronoField.MICRO_OF_DAY) - start.getLong(ChronoField.MICRO_OF_DAY);
            
+           System.out.println("W1 request sent at: " + start/*java.time.LocalTime.now()*/);
 
-           System.out.println("W1 response received at: " + java.time.LocalTime.now());
+           System.out.println("W1 response received at: " + end/*java.time.LocalTime.now()*/);
            capitalizedSentence = clientSentence.toUpperCase() + '\n';
 
-           outToClient.writeBytes(data);
+           outToClient.writeBytes(capitalizedSentence);
+           
+           
+           System.out.println("W1 delay: " + difference + "ms");
+
+          
         }
       }
     }
